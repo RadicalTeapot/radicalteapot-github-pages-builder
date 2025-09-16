bcd6f22c3ef1f5736cfe1f701e69c78e5b5f7c49
RadicalTeapot <35329469+RadicalTeapot@users.noreply.github.com>
2025-09-16 22:34:05 +0200

    Add linux test runners

--- testing/test_all.sh
+++ testing/test_all.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-printf '\n%s' "=== Running get-files-to-publish tests ==="
+printf '\n%s' "=== Running get-files-to-publish tests ===" >&2
 ./get-files-to-publish_command/test.sh
-printf '%s\n' "=== get-files-to-publish done ==="
+printf '%s\n' "=== get-files-to-publish done ===" >&2
 
-printf '\n%s' "=== Running frontmatter-parser tests ==="
+printf '\n%s' "=== Running frontmatter-parser tests ===" >&2
 ./frontmatter-parser_command/test.sh
-printf '%s\n' "=== frontmatter-parser done ==="
+printf '%s\n' "=== frontmatter-parser done ===" >&2
 
-printf '\n%s' "=== Running extract-links tests ==="
+printf '\n%s' "=== Running extract-links tests ===" >&2
 ./extract-links_command/test.sh
-printf '%s\n' "=== extract-links done ==="
+printf '%s\n' "=== extract-links done ===" >&2
 
-printf '\n%s' '=== Running validate-markdown-content tests ==='
+printf '\n%s' '=== Running validate-markdown-content tests ===' >&2
 ./validate-markdown-content_command/test.sh
-printf '%s\n' '=== validate-markdown-content done ==='
+printf '%s\n' '=== validate-markdown-content done ===' >&2
