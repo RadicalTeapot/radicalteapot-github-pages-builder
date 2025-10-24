ded84da28c100c911489620c8aa10b16395b3fa3
Mathias Capdet <mathias.capdet@tescan.com>
2025-10-24 15:42:05 +0200

    Fix command and tests

--- testing/get-files-to-publish_command/test.sh
+++ testing/get-files-to-publish_command/test.sh
@@ -35,13 +35,8 @@ test_runner() {
 
 test_empty_directory_no_options() {
     local _result
-    _result="$("$_command" "$_test_dir")"
-
-    if ! assert_success $?; then
-        return 1
-    fi
-
-    if ! assert_empty "$_result"; then
+    _"$_command" "$_test_dir" 2>/dev/null
+    if ! assert_failure $? "Expected failure when pointing to an empty directory"; then
         return 1
     fi
 }
@@ -52,13 +47,13 @@ test_empty_file_no_options() {
     touch "$_path"
 
     local _result
-    _result="$("$_command" "$_test_dir")"
+    _result="$("$_command" "$_test_dir" 2>/dev/null)"
 
-    if ! assert_success $?; then
+    if ! assert_success $? "Expected success when pointing to an empty file"; then
         return 1
     fi
 
-    if ! assert_eq "$_result" "$_path"; then
+    if ! assert_empty "$_result" "Expected empty output when pointing to an empty file and not ignoring publish state, got '$_result'"; then
         return 1
     fi
 }
@@ -69,13 +64,13 @@ test_empty_file_with_publish_option() {
     touch "$_path"
 
     local _result
-    _result=$("$_command" --only-published "$_test_dir")
+    _result=$("$_command" --ignore-publish-state "$_test_dir")
 
-    if ! assert_success $?; then
+    if ! assert_success $? "Expected success when pointing to an empty file and ignoring publish state"; then
         return 1
     fi
 
-    if ! assert_empty "$_result"; then
+    if ! assert_eq "$_result" "$_path" "Expected file path when ignoring publish state"; then
         return 1
     fi
 }
@@ -91,11 +86,11 @@ EOF
     local _result
     _result=$("$_command" "$_test_dir")
 
-    if ! assert_success $?; then
+    if ! assert_success $? "Expected success when pointing to a file with publish: false"; then
         return 1
     fi
 
-    if ! assert_eq "$_result" "$_path"; then
+    if ! assert_empty "$_result" "Expected empty output when pointing to a file with publish: false and not ignoring publish state, got '$_result'"; then
         return 1
     fi
 }
@@ -110,13 +105,13 @@ publish: false
 EOF
 
     local _result
-    _result=$("$_command" --only-published "$_test_dir")
+    _result=$("$_command" --ignore-publish-state "$_test_dir")
 
-    if ! assert_success $?; then
+    if ! assert_success $? "Expected success when pointing to a file with publish: false and ignoring publish state"; then
         return 1
     fi
 
-    if ! assert_empty "$_result"; then
+    if ! assert_eq "$_result" "$_path" "Expected file path when pointing to a file with publish: false and ignoring publish state"; then
         return 1
     fi
 }
@@ -142,7 +137,7 @@ EOF
     fi
 }
 
-test_file_with_publish_true_with_option() {
+test_file_with_publish_true_with_no_option() {
     local _path
     _path="${_test_dir}/file.md"
     cat <<-'EOF' > "$_path"
@@ -152,21 +147,21 @@ publish: true
 EOF
 
     local _result
-    _result=$("$_command" --only-published "$_test_dir")
+    _result=$("$_command" "$_test_dir")
 
-    if ! assert_success $?; then
+    if ! assert_success $? "Expected success when pointing to a file with publish: true"; then
         return 1
     fi
 
-    if ! assert_eq "$_result" "$_path"; then
+    if ! assert_eq "$_result" "$_path" "Expected file path when pointing to a file with publish: true"; then
         return 1
     fi
 }
 
-test_runner test_empty_directory_no_options "Expecting no output"
-test_runner test_empty_file_no_options "Expecting file path"
-test_runner test_empty_file_with_publish_option "Expecting no output"
-test_runner test_file_with_publish_false_no_option "Expecting file path"
-test_runner test_file_with_publish_false_with_option "Expecting no output"
+test_runner test_empty_directory_no_options "Test empty directory handling"
+test_runner test_empty_file_no_options "Expecting no output"
+test_runner test_empty_file_with_publish_option "Expecting file path"
+test_runner test_file_with_publish_false_no_option "Expecting no output"
+test_runner test_file_with_publish_false_with_option "Expecting file path"
 test_runner test_file_with_publish_true_with_no_option "Expecting file path"
-test_runner test_file_with_publish_true_with_option "Expecting file path"
+test_runner test_file_with_publish_true_with_no_option "Expecting file path"
